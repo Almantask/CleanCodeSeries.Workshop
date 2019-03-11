90ff60dfac6ca4715386d0b181e64b42832c72ad
Kaisinel <almantusk@gmail.com>
2019-03-11 20:14:12 +0200

    A few more bad naming examples

---
+++ src/CleanCodeSeries.Workshop.Lesson1.EasyToUnderstandCode/BadNaming/CarCache.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodeSeries.Workshop.Lesson1.EasyToUnderstandCode.BadNaming
+{
+    class CarCache
+    {
+        private List<Car> _cars;
+
+        public CarCache()
+        {
+            _cars = new List<Car>();
+        }
+
+        public void AddCar(Car car, int at)
+        {
+            _cars.Insert(at, car);
+        }
+
+        public Car RetrieveCar(int i)
+        {
+            return _cars[i];
+        }
+
+        public Car SetCar(Car car)
+        {
+            return car;
+        }
+
+        public void DestroyCar(Car car)
+        {
+            _cars.Remove(car);
+        }
+
+        public void DestroyCar(int i)
+        {
+            _cars.RemoveAt(i);
+        }
+
+    }
+}
---
+++ src/CleanCodeSeries.Workshop.Lesson1.EasyToUnderstandCode/BadNaming/CarFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using CleanCodeSeries.Workshop.Lesson1.EasyToUnderstandCode.PoorCommenting;
+
+namespace CleanCodeSeries.Workshop.Lesson1.EasyToUnderstandCode.BadNaming
+{
+    public class CarFactory
+    {
+        public Car SetCar()
+        {
+            return new Car(100, 50, "F", 5, new Vector3(0, 0, 0));
+        }
+
+
+    }
+}

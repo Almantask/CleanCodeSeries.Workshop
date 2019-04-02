7f99c6d3f00ac1385c39ddf3907d9a85fb2b5807
Almantas <almantusk@gmail.com>
2019-04-02 15:55:20 +0300

    Split to files

---
+++ src/CleanCodeSeries.Workshop.Lesson3.EasyOOP/Commander.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Randomly generated soldier go to war
+ * They shoot at each other and get promotoed
+ * Soldier->Leutenant->Commander
+ * The moment a soldier reaches Commander rank, war is over.
+ * And all soldiers dismiss.
+ * Else, they shoot again.
+ */
+namespace CleanCodeSeries.Workshop.Lesson3.EasyOOP
+{
+    public class Commander:Soldier
+    {
+        public Commander(Soldier soldier) : base(soldier.Person, soldier.HP, soldier.XP) { }
+    }
+}
---
+++ src/CleanCodeSeries.Workshop.Lesson3.EasyOOP/Person.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Randomly generated soldier go to war
+ * They shoot at each other and get promotoed
+ * Soldier->Leutenant->Commander
+ * The moment a soldier reaches Commander rank, war is over.
+ * And all soldiers dismiss.
+ * Else, they shoot again.
+ */
+namespace CleanCodeSeries.Workshop.Lesson3.EasyOOP
+{
+    public class Person
+    {
+        public string Name { get; }
+        public int Age { get; }
+        public float Weight { set; get; }
+        public float Height { set; get; }
+
+        public Person(string name, int age, float weight, float height)
+        {
+            Name = name;
+            Age = age;
+            Height = weight;
+            Weight = height;
+        }
+    }
+}
